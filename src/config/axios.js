db3842abf22382234b80d4afcdfadf9f1177d255
CheeRanidu <nayonranidulochana@gmail.com>
2023-09-23 09:44:18 +0530

    proxy

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,11 +1,5 @@
 import axios from "axios";
 export default axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-  headers: {
-    "Access-Control-Allow-Origin": "*", // Replace '*' with your allowed origin if needed
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    "Content-Type": "application/json", // Set your content type here
-  },
 });
 //axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
